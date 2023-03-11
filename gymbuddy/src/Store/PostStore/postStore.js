69f67929eccad67c9f08194ce7e45bf978a755a6
buli-dot <72310468+buli-dot@users.noreply.github.com>
2023-03-11 16:49:02 +0100

    made postform into component for reusabilty

--- gymbuddy/src/Store/PostStore/postStore.js
+++ gymbuddy/src/Store/PostStore/postStore.js
@@ -1,6 +1,4 @@
 import { action, makeObservable, observable } from "mobx";
-import PostComponent from "../../containers/home/components/PostComponent";
-
 class PostStoreImpl {
   posts = [];
   constructor() {
@@ -15,11 +13,6 @@ class PostStoreImpl {
   }
   addComment(comment) {
     this.posts.comments.push(comment);
-  }
-  mapPosts() {
-    this.posts.map((x) => {
-      return console.log(x);
-    });
   }
 }
 export const PostStore = new PostStoreImpl();
